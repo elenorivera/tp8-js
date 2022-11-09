ca110ca088c6896119aadd370ed3197fa40b63b1
elenorivera <elenorivera509@gmail.com>
2022-11-09 03:02:21 +0000

    image folder

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 /* JavaScript code for memory card game */
+
+window.onload = function () {
+ cardList = document.getElementByClassName("card"); // collection of cards
+  
+  cardCount = cardList.length; // how many cards are on the table
+  
+  for (c=0; c< cardCount; c++) {
+    cardList[c].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+}
